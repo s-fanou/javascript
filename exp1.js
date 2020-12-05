034ebc2fedfd30e4a30b31b23eebf9020224249b
s-fanou <sotiris.fanou@gmail.com>
2020-12-05 17:01:08 +0200

    removes comments

--- exp1.js
+++ exp1.js
@@ -4,7 +4,5 @@ $(document).ready(function () {
 
     $("#span1").text("This is text assigned from jQuery");
 
-    //this is a git test this should exist only in exp1 branch
-    // merge this into master
 
 }); // ends document.ready
