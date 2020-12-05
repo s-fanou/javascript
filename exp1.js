084e5d4393ec048c8091911e512042438add4fd1
s-fanou <sotiris.fanou@gmail.com>
2020-12-05 17:08:13 +0200

    adds button handler

--- exp1.js
+++ exp1.js
@@ -4,5 +4,10 @@ $(document).ready(function () {
 
     $("#span1").text("This is text assigned from jQuery");
 
+    $("#btn1").click(function() {
+        $("#span1").text("The button has been clicked!");
+
+    });
+
 
 }); // ends document.ready
