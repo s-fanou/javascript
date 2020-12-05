cbd6e1f70d3d656ac43f3e09049f2c155a439dc1
s-fanou <sotiris.fanou@gmail.com>
2020-12-05 16:50:53 +0200

    comment for testing merging

--- exp1.js
+++ exp1.js
@@ -5,5 +5,6 @@ $(document).ready(function () {
     $("#span1").text("This is text assigned from jQuery");
 
     //this is a git test this should exist only in exp1 branch
+    // merge this into master
 
 }); // ends document.ready
