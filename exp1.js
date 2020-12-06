81a6e67cbf974100509c3dbc2b6f9f9d82bf0b4e
s-fanou <sotiris.fanou@gmail.com>
2020-12-06 11:53:52 +0200

    testing a semi colon

--- exp1.js
+++ exp1.js
@@ -1,13 +1,21 @@
 /* JS file for exp1.html*/
 
 $(document).ready(function () {
+  $("#span1").text("This is text assigned from within");
 
-    $("#span1").text("This is text assigned from jQuery");
-
-    $("#btn1").click(function() {
-        $("#span1").text("The button has been clicked!");
-
-    });
-
-
+  $("#btn1").click(function () {
+    //$("#span1").text("The button has been clicked!");
+    var x = car.myFunction("sot", "fanou");
+    document.getElementById("span1").innerHTML = x;
+  });
 }); // ends document.ready
+
+var car = {}; //this is another way to say new Object()
+
+car.type = "Fiat";
+car.model = "500";
+car.color = "white";
+car.myFunction = function (param1, param2) {
+  //function does something
+  return param1 + " " + param2;
+}; //If I remove this ';' it still works !!!
