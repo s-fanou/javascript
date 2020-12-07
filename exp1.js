8605d44fbb57322b4c39155f4d71fd4d5bff797e
s-fanou <sotiris.fanou@gmail.com>
2020-12-07 20:56:07 +0200

    adds derived class female

--- exp1.js
+++ exp1.js
@@ -1,28 +1,10 @@
 /* JS file for exp1.html*/
 
-$(document).ready(function () {
-
-  $("#span1").text("This is text assigned from jQuery");
-
-  $("#btn1").click(function () {
-    //$("#span1").text("The button has been clicked!");
-    //var xout = "button was clicked";
-    document.getElementById("span1").innerHTML = sot.firstname + " " + sot.lastname;
-    sot.firstname = "Lakis";
-    sot.lastname = "Antoniou";
-
-    $("#txtArea1").val(sot.firstname + " " + sot.lastname);
-
-
-  });
-
-}); // ends document.ready
-
 class person {
 
-    constructor(fname, lname){
-        this.fname = fname;
-        this.lname = lname;
+    constructor(){
+        this.fname;
+        this.lname;
     }
 
     get firstname (){
@@ -43,5 +25,46 @@ class person {
 
 } // ends class person
 
-var sot = new person("Sot", "Fanou");
+class female extends person{
+    constructor(){
+        super(); //I must call super() even though person's constructor takes no arguments
+        this._gender; //this property is by default public
+    }
+
+    get gender(){
+        return this._gender;        
+    }
+
+    set gender(mf){
+        this._gender = mf;
+    }
+
+} // ends class female
+
+var sot = new person();
+var maryana = new female;
+
+
+$(document).ready(function () {
+
+  
+  $("#btn1").click(function () {
+    //$("#span1").text("The button has been clicked!");
+    //var xout = "button was clicked";
+    //document.getElementById("span1").innerHTML = sot.firstname + " " + sot.lastname;
+
+    sot.firstname = "Lakis";
+    sot.lastname = "Antoniou";
+    $("#txtArea1").val(sot.firstname + " " + sot.lastname);
+
+    maryana.firstname = "Maryana";
+    maryana.lastname = "Ofitserova";
+    maryana._gender = "female";//notice here that I can assign to _gender without the setter
+    
+    $("#span1").text(maryana.firstname +" "+ maryana.lastname +" "+ maryana.gender);
+
+  });
+
+}); // ends document.ready
+
 
