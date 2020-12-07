07661973f9d54d9e91c1f325564569f58d799309
s-fanou <sotiris.fanou@gmail.com>
2020-12-07 21:10:13 +0200

    adds comment

--- exp1.js
+++ exp1.js
@@ -1,53 +1,47 @@
 /* JS file for exp1.html*/
 
 class person {
+  constructor() {
+    this.fname;//all the properties must be declared in the constructor
+    this.lname;
+  }
 
-    constructor(){
-        this.fname;
-        this.lname;
-    }
+  get firstname() {
+    return this.fname;
+  }
 
-    get firstname (){
-        return this.fname;
-    }
+  get lastname() {
+    return this.lname;
+  }
 
-    get lastname(){
-        return this.lname;
-    }
+  set firstname(name) {
+    this.fname = name;
+  }
 
-    set firstname (name){
-        this.fname = name;
-    }
-
-    set lastname(name){
-        this.lname = name;
-    }
-
+  set lastname(name) {
+    this.lname = name;
+  }
 } // ends class person
 
-class female extends person{
-    constructor(){
-        super(); //I must call super() even though person's constructor takes no arguments
-        this._gender; //this property is by default public
-    }
+class female extends person {
+  constructor() {
+    super(); //I must call super() even though person's constructor takes no arguments
+    this._gender; //this property is by default public
+  }
 
-    get gender(){
-        return this._gender;        
-    }
+  get gender() {
+    return this._gender;
+  }
 
-    set gender(mf){
-        this._gender = mf;
-    }
-
+  set gender(mf) {
+    this._gender = mf;
+  }
 } // ends class female
 
 var sot = new person();
-var maryana = new female;
-
+var maryana = new female();
 
 $(document).ready(function () {
-
-  
   $("#btn1").click(function () {
     //$("#span1").text("The button has been clicked!");
     //var xout = "button was clicked";
@@ -59,12 +53,10 @@ $(document).ready(function () {
 
     maryana.firstname = "Maryana";
     maryana.lastname = "Ofitserova";
-    maryana._gender = "female";//notice here that I can assign to _gender without the setter
-    
-    $("#span1").text(maryana.firstname +" "+ maryana.lastname +" "+ maryana.gender);
+    maryana._gender = "female"; //notice here that I can assign to _gender without the setter
 
+    $("#span1").text(
+      maryana.firstname + " " + maryana.lastname + " " + maryana.gender
+    );
   });
-
 }); // ends document.ready
-
-
