b647d2f5001632a26dda6503c167e4c41bb2758f
s-fanou <sotiris.fanou@gmail.com>
2020-12-07 20:31:46 +0200

    adds class person

--- exp1.js
+++ exp1.js
@@ -6,8 +6,42 @@ $(document).ready(function () {
 
   $("#btn1").click(function () {
     //$("#span1").text("The button has been clicked!");
-    var xout = "button was clicked";
-    document.getElementById("span1").innerHTML = xout;
+    //var xout = "button was clicked";
+    document.getElementById("span1").innerHTML = sot.firstname + " " + sot.lastname;
+    sot.firstname = "Lakis";
+    sot.lastname = "Antoniou";
+
+    $("#txtArea1").val(sot.firstname + " " + sot.lastname);
+
+
   });
 
 }); // ends document.ready
+
+class person {
+
+    constructor(fname, lname){
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    get firstname (){
+        return this.fname;
+    }
+
+    get lastname(){
+        return this.lname;
+    }
+
+    set firstname (name){
+        this.fname = name;
+    }
+
+    set lastname(name){
+        this.lname = name;
+    }
+
+} // ends class person
+
+var sot = new person("Sot", "Fanou");
+
