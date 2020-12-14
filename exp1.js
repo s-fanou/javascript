87bb6a5df7f582a9ae3bf4a54d4dd442a29a9f27
s-fanou <sotiris.fanou@gmail.com>
2020-12-14 19:32:11 +0200

    white space removal

--- exp1.js
+++ exp1.js
@@ -2,12 +2,19 @@
 
 $(document).ready(function () {
 
-  $("#span1").text("This is text assigned from jQuery");
+  $("#span1").text("This is text assigned from jQuery to span1");
+
+  $("#span2").text("This is text assigned from jQuery to span2");
 
   $("#btn1").click(function () {
+    
     //$("#span1").text("The button has been clicked!");
-    var xout = "button was clicked";
-    document.getElementById("span1").innerHTML = xout;
+
+    //var xout = "button was clicked";
+    //document.getElementById("span1").innerHTML = xout;
+
+    $("#txtArea1").val("Text assigned to txtArea1 by clicking the button");
+
   });
 
 }); // ends document.ready
