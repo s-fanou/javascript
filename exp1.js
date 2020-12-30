d7bb3185070cc24ed9c96b28eda24adfb8998daf
s-fanou <sotiris.fanou@gmail.com>
2020-12-30 17:25:49 +0200

    adds extraParams()

--- exp1.js
+++ exp1.js
@@ -7,14 +7,29 @@ $(document).ready(function () {
   $("#span2").text("This is text assigned from jQuery to span2");
 
   $("#btn1").click(function () {
-    
+
     //$("#span1").text("The button has been clicked!");
 
     //var xout = "button was clicked";
     //document.getElementById("span1").innerHTML = xout;
 
-    $("#txtArea1").val("Text assigned to txtArea1 by clicking the button");
+    $("#txtArea1").val("Stefanie Fanou");
+
+    //x = extraParams(2);
+    //$("#txtArea1").val("extraParams() returned " + x.toString());
+
 
   });
 
 }); // ends document.ready
+
+function extraParams(a, b){
+
+  if(b === 3){
+    return b;
+
+  } else{
+    return a;
+  }
+
+}
