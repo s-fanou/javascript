483740115c773e2f3db7db56fe04be0819027c59
s-fanou <sotiris.fanou@gmail.com>
2021-01-28 13:10:58 +0000

    hw instructions

--- exp1.js
+++ exp1.js
@@ -1,22 +1,32 @@
-// this is marios first lesson 27 01 2021
+// this is marios first homework 28 01 2021
 
-/* i have no idea what to write....
-this game is inspired by the battle cats.  */
 
 $(document).ready(function () {
 
-  var level = 0;
-  var myvar2 = "maryana";
-
+/* Marie you have to work only below
+in the click function */
   $("#btn1").click(function () {
-    
-    level = level + 1;
 
-    $("#txt1").val("The level is " + level);
+    /* homework instructions: You must create 2 variables 
+    using the keyword var. You can name the variables whatever
+    you want. 
+    (1)In the first variable I want you to put in 
+    the phrase "my name is Marios." and then display 
+    the value inside that variable in the top text-box.
+    (2) In the second variable I want you to put the 
+    result of the addition 4 + 6 and then display the 
+    result of this addition the second text-box */
 
-    // this is the second textbox
-    //$("#txtArea1").val("");
-    $("#span1").text(myvar2);
+    // this is the top text-box
+    $("#txt1").val("");
+
+    // this is the second text-box
+    $("#txtArea1").val("");
+
+    // this is the red span area
+    $("#span1").text("");
+
   });
+
 
 }); // ends document.ready
