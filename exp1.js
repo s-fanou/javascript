e10699d1cdb8bef2a6245a0bce8425c5e3b50cf9
s-fanou <sotiris.fanou@gmail.com>
2021-02-14 17:16:55 +0200

    part 1 of lesson

--- exp1.js
+++ exp1.js
@@ -2,28 +2,34 @@
 
 /*  this game is inspired by the battle cats.  */
 
-$(document).ready(function () 
-{
+$(document).ready(function () {
   var level = 1;
   $("#txtLevel").val("Your level: " + level);
 
   var gamePoints = 0;
   $("#txtPoints").val("Your points: " + gamePoints);
 
+  $("#btnAnswer").click(function () {
+    var numb1 = $("#txtNumber1").val();
+    var numb2 = $("#txtNumber2").val();
 
+    var computerAnswer = parseInt(numb1) + parseInt(numb2);
 
-  $("#btnAnswer").click(function () 
-  {
+    var UsersAnswer = $("#txtaAnswer").val();
 
-    var numb1 =  $("#txtNumber1").val();
-    var numb2 =  $("#txtNumber2").val();
+    console.log("the value of computerAnswer is: " + computerAnswer);
+    console.log("the value of UsersAnswer is: " + UsersAnswer);
 
+    if (computerAnswer == UsersAnswer) {
+      alert("YOUR ANSWER IS CORRECT!!!");
+    } else {
+      alert("YOUR ANSWER IS WRONG!!!");
+    }
 
     $("#txtLevel").val("The level is " + level);
 
     // this is the second textbox
     //$("#txtArea1").val("");
     // $("#span1").text(myvar2);
-
   });
 }); // ends document.ready
