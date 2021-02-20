303caebe21bace8aa55b4c8c221d286144f4d987
Sotiris Fanou <sotiris.fanou@gmail.com>
2021-02-20 15:18:25 +0200

    something

--- exp1.js
+++ exp1.js
@@ -1,21 +1,17 @@
 /* JS file for exp1.html*/
 
 $(document).ready(function () {
-  $("#span1").text("This is text assigned from within");
+
 
   $("#btn1").click(function () {
-    //$("#span1").text("The button has been clicked!");
-    var x = car.myFunction("sot", "fanou");
-    document.getElementById("span1").innerHTML = x;
+
+    var answer1 = 3*6;
+    var answer2 = (20*10) / 5;
+
+    var finalAns = answer1 + answer2;
+
+    $("#span1").text(finalAns);
+
   });
 }); // ends document.ready
 
-var car = {}; //this is another way to say new Object()
-
-car.type = "Fiat";
-car.model = "500";
-car.color = "white";
-car.myFunction = function (param1, param2) {
-  //function does something
-  return param1 + " " + param2;
-}; //If I remove this ';' it still works !!!
